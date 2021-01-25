097ba7dc98ffd1b1783b972d49e3173ac49b4090
Nyah Anderson <nkarpaiya@yahoo.co.uk>
2021-01-25 16:06:09 +0000

    First Test

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const http = require('http');
+var path = require("path");
+var fs = require("fs");
+let cors = require('cors')
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+let db;
+
+MongoClient.connect('mongodb+srv://naDB:THISISMYAPSSWORD1010$@cw2cluster.rhjxe.mongodb.net/', (err, client) => {
+db = client.db('Webstore')
+});
+
+app.param('collectionName', (req, res, next, collectionName) => {
+req.collection = db.collection(collectionName)
+return next()
+});
+
+app.get('/collection/:collectionName', (req, res, next) => {
+req.collection.find({}).toArray((e, results) => {
+if (e) return next(e)
+res.send(results)
+}
+)
+}); 
+
+app.post('/collection/:collectionName', (req, res, next) => {
+req.collection.insert(req.body, (e, results) => {
+if (e) return next(e)
+res.send(results.ops)})
+});
+
+
+app.use(function(req, res, next) {
+    console.log("Request IP: " + req.url);
+    console.log("Request date: " + new Date());
+    next();
+    });
+
+    app.use(function(req,res,next){
+        var filePath = path.join(__dirname,"static", req.url);
+    
+            fs.stat(filePath, function(err, fileInfo) {
+            if (err) { 
+            next(); 
+            return; 
+            }
+            if (fileInfo.isFile()) res.sendFile(filePath);
+            else next();
+        });
+    });
+    
+    app.use(function(req, res) {   
+        res.status(404);
+        res.send("File not found!");
+        });
+    
+
+app.listen(3000, function () {
+    console.log("Express server has started");
+
+});
