076585352c1789f5730c095501e0e93d25c95537
Nyah Anderson <nkarpaiya@yahoo.co.uk>
2021-01-26 16:22:42 +0000

    Format

--- app.js
+++ app.js
@@ -14,67 +14,68 @@ app.use(express.json());
 let db;
 
 MongoClient.connect('mongodb+srv://naDB:THISISMYAPSSWORD1010$@cw2cluster.rhjxe.mongodb.net/', (err, client) => {
-db = client.db('Webstore')
+    db = client.db('Webstore')
 });
 
 app.param('collectionName', (req, res, next, collectionName) => {
-req.collection = db.collection(collectionName)
-return next()
+    req.collection = db.collection(collectionName)
+    return next()
 });
 
 app.get('/collection/:collectionName', (req, res, next) => {
-req.collection.find({}).toArray((e, results) => {
-if (e) return next(e)
-res.send(results)
-}
-)
-}); 
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next(e)
+        res.send(results)
+    }
+    )
+});
 
 app.post('/collection/:collectionName', (req, res, next) => {
-req.collection.insert(req.body, (e, results) => {
-if (e) return next(e)
-res.send(results.ops)})
+    req.collection.insert(req.body, (e, results) => {
+        if (e) return next(e)
+        res.send(results.ops)
+    })
 });
 
 const ObjectID = require('mongodb').ObjectID;
 app.put('/collection/:collectionName/:id', (req, res, next) => {
-req.collection.update({ _id: new ObjectID(req.params.id) },
-{ $set: req.body },
-{ safe: true, multi: false },
-(e, result) => {
-if (e) return next(e)
-res.send((result.result.n === 1) ?
-{msg: 'success'} : { msg: 'error'})
-})
+    req.collection.update({ _id: new ObjectID(req.params.id) },
+        { $set: req.body },
+        { safe: true, multi: false },
+        (e, result) => {
+            if (e) return next(e)
+            res.send((result.result.n === 1) ?
+                { msg: 'success' } : { msg: 'error' })
+        })
 })
 
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     console.log("Request IP: " + req.url);
     console.log("Request date: " + new Date());
     next();
-    });
+});
 
-    app.use(function(req,res,next){
-        var filePath = path.join(__dirname,"static", req.url);
-    
-            fs.stat(filePath, function(err, fileInfo) {
-            if (err) { 
-            next(); 
-            return; 
-            }
-            if (fileInfo.isFile()) res.sendFile(filePath);
-            else next();
-        });
+app.use(function (req, res, next) {
+    var filePath = path.join(__dirname, "static", req.url);
+
+    fs.stat(filePath, function (err, fileInfo) {
+        if (err) {
+            next();
+            return;
+        }
+        if (fileInfo.isFile()) res.sendFile(filePath);
+        else next();
     });
-    
-    app.use(function(req, res) {   
-        res.status(404);
-        res.send("File not found!");
-        });
-    
-        const port = process.env.PORT || 3000
-        app.listen(port)
+});
+
+app.use(function (req, res) {
+    res.status(404);
+    res.send("File not found!");
+});
+
+const port = process.env.PORT || 3000
+app.listen(port)
 
 //app.listen(3000, function () {
     //console.log("Express server has started");
