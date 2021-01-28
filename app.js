2f44225c5efa9c9d9bb97b9985f4c5d577dad3ac
Nyah Anderson <nkarpaiya@yahoo.co.uk>
2021-01-28 22:16:12 +0000

    Update

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+    console.log("Request IP: " + req.url);
+    console.log("Request date: " + new Date());
+    next();
+});
+
 let db;
 
 MongoClient.connect('mongodb+srv://naDB:THISISMYAPSSWORD1010$@cw2cluster.rhjxe.mongodb.net/', (err, client) => {
@@ -58,17 +64,9 @@ app.put('/collection/:collectionName/:id', (req, res, next) => {
         })
 })
 
-/*
-app.use(function (req, res, next) {
-    console.log("Request IP: " + req.url);
-    console.log("Request date: " + new Date());
-    next();
-});
-*/
-app.use(function(request, response) { // middleware
-    console.log("In comes a request to: " + request.url);
-    //response.end("Hello, world!");
-    });
+
+
+
 
 app.get('/', (req, res, next) => {
         res.send('Select a collection, e.g., /collection/LessonInfo')
