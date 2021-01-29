64010d4249f918d7fa986d78c8bb764fad8c6b12
Nyah Anderson <nkarpaiya@yahoo.co.uk>
2021-01-29 00:33:37 +0000

    Check

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get('/collection/:collectionName', (req, res, next) => {
     )
 });
 
-app.post('/collection/:collectionName', (req, res, next) => {
+app.post('/collection/OrderInfo', (req, res, next) => {
     req.collection.insert(req.body, (e, results) => {
         if (e) return next(e)
         res.send(results.ops)
