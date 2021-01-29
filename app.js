20f0512ad1797bd2d91aa8cf2f2ec8a92f8d4a1a
Nyah Anderson <nkarpaiya@yahoo.co.uk>
2021-01-29 02:46:53 +0000

    update

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post('/collection/:collectionName', (req, res, next) => {
     })
 });
 
-/*
+
 const ObjectID = require('mongodb').ObjectID;
 app.get('/collection/:collectionName/:id', (req, res, next) => {
     req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
@@ -53,7 +53,7 @@ app.get('/collection/:collectionName/:id', (req, res, next) => {
         res.send(result)
     })
 })
-*/
+
 
 //UPDATE spaces in Lesson
 app.put('/collection/:collectionName/:id', (req, res, next) => {
